408c9fe15037c6edf13d15eb44a53d2e33a84938
thebengali <peaksanjaym@gmail.com>
2025-08-19 23:58:20 +0530

    Move components into app/components and fix relative imports for Vercel

---
+++ components/Quiz.tsx
@@ -0,0 +1,8 @@
+export default function Quiz(){
+  return (
+    <div style={{padding:24, maxWidth:760, margin:"0 auto"}}>
+      <h1>Quiz Time!</h1>
+      <p>Weighted scoring + banter coming soon...</p>
+    </div>
+  )
+}
