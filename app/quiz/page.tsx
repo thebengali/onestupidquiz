4f62909d7f8b5b7220ad715a658b53079ed2604b
thebengali <peaksanjaym@gmail.com>
2025-08-19 16:18:23 +0530

    Fix import path: use relative path for components/Quiz

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,5 +1,5 @@
-import Quiz from "@/components/Quiz";
+import Quiz from "../../components/Quiz"
 
-export default function Home() {
-  return <Quiz />;
+export default function QuizPage(){
+  return <Quiz />
 }
